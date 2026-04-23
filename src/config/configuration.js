785a6e529a4d58fe8bd580feaa594d351b5f8414
Ananay Gupta <dev@bettermystay.com>
2026-04-23 10:37:04 +0530

    number update

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -22,6 +22,7 @@ module.exports = {
     COMPANY_X_HANDLE_URL:"",
     COMPANY_DISCORD_HANDLE_URL:"",
     BOOK_DEMO_URL : "",
+    COMPANY_PHONE : "919119772488",
     WHATSAPP_CHAT_NUMBER : "919119772488",
     WHATSAPP_LIST_MESSAGE : "Hello! I'm interested in inquiring about Hosting my property with you. Please let me know the best way to proceed. Thanks!",
     WHATSAPP_CHAT_MESSAGE : "Hello! I'm interested in inquiring about a stay at one of your properties. Please let me know the best way to proceed with a booking. Thanks!",
